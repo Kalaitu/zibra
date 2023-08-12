615c912c7e799d22e4d4c0732e2056b719ec9b29
Nana <nanasupriatnakursus@gmail.com>
2023-08-12 21:10:03 +0700

    udpate layout
    udpate layout

--- app/Views/Manager/Layout.php
+++ app/Views/Manager/Layout.php
@@ -136,7 +136,6 @@
   <!-- / Layout wrapper -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css" /> -->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
   <script src="<?= base_url('assets/vendor/libs/jquery/jquery.js') ?>"></script>
   <script src="<?= base_url('assets/vendor/libs/popper/popper.js') ?>"></script>
@@ -167,6 +166,7 @@
       $('.dataTables_filter').addClass('pull-left');
       $('.dataTables_paginate').addClass('pull-left');
     });
+
     $(document).ready(function() {
       $('#tableproduk').DataTable({
         paging: false,
